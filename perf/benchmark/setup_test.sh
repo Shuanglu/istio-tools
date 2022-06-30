d3965c02e5d1bbc40b6d0e93a2b23f9247c93f30
Shuang Lu <shuanglu@tubi.tv>
2022-06-30 20:00:45 +0800

    add envoy sidecar kustomization

--- perf/benchmark/setup_test.sh
+++ perf/benchmark/setup_test.sh
@@ -110,7 +110,10 @@ function run_test() {
             . > "${TMPDIR}/${NAMESPACE}.yaml"
     fi
   echo "Wrote file ${TMPDIR}/${NAMESPACE}.yaml"
-
+  sed s/NAMESPACE/${NAMESPACE}/g;s/ENVOY/{$WD}\/kusotmization\/components\/envoy  kusotmization/kusotmization.yaml > "${TMPDIR}"/kustomization.yaml
+  
+  # remove stdio rules
+  kustomize build "${TMPDIR}/kusotmization.yaml" | kubectl apply -n "${NAMESPACE}" -f - || true
   # remove stdio rules
   kubectl apply -n "${NAMESPACE}" -f "${TMPDIR}/${NAMESPACE}.yaml" || true
   kubectl rollout status deployment fortioclient -n "${NAMESPACE}" --timeout=5m
