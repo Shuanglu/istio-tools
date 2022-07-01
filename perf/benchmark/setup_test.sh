2990b042ee6f02a313af0e0a78167e4f978299cb
Shuang Lu <shuanglu@tubi.tv>
2022-07-01 16:03:39 +0800

    cleanup the data folder

--- perf/benchmark/setup_test.sh
+++ perf/benchmark/setup_test.sh
@@ -122,6 +122,7 @@ function run_test() {
   kubectl rollout status deployment fortioclient -n "${NAMESPACE}" --timeout=5m
   kubectl rollout status deployment fortioserver -n "${NAMESPACE}" --timeout=5m
   echo "${TMPDIR}/${NAMESPACE}.yaml"
+  rm -rf "${TMPDIR}/kustomization"
 }
 
 for ((i=1; i<=$#; i++)); do
